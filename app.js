bd3dcf73e90fc3fc90574de1bc035a6b80290ade
Alexander Martins <alex.rodrigo.martins@gmail.com>
2025-01-31 16:18:09 -0300

    Docs (javascript): Escrever o arquivo app.js com as funções e lógica para resolver o desafio

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigo = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value.trim();
+    //validar entrada vazia
+    if (nome === '') {
+        alert('Por favor, insira um nome antes de adicionar');
+    } else {
+        //validar se tem nome repetido
+        if (amigo.includes(nome)) {
+            let resposta = prompt('Esse nome já existe na sua lista de amigos secretos, quer adicionar assim mesmo ? [S]im ou [N]ão').toUpperCase();
+            if (resposta === 'S') {
+                amigo.push(nome);
+            }
+        } else {
+            amigo.push(nome);
+        }
+    }
+    limparCampo();
+    criarLista();
+    }
+
+function limparCampo() {
+    let nome = document.getElementById('amigo');
+    nome.value = '';
+}
+
+
+function criarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0 ; i < amigo.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigo[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigo.length == 0 ) {
+        alert('Precisa colocar o nome de seus amigos para sortear o amigo secreto');
+    } else {
+        let IndiceAleatorio = parseInt(Math.floor(Math.random() * amigo.length));
+        let nomeSorteado = amigo[IndiceAleatorio];
+        let amigoSecreto = document.getElementById('resultado');
+        amigoSecreto.innerHTML = nomeSorteado;
+    }
+}
